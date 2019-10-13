8b123ec5588459c1e9347e3557a41b3b46f44283
kbab6aoo <kbab6aoo@hotmail.com>
2019-10-13 12:21:57 +0100

    version provider changed due to a bug in terraform

--- hsbc-aws-ec2/provider.tf
+++ hsbc-aws-ec2/provider.tf
@@ -1,7 +1,7 @@
 # Setup our AWS Provider..!
 
 provider "aws" {
-  version = "~> 2.7"
+  version = "~> 2.22.0"
 
   shared_credentials_file = "/../../.aws/credentials"
   profile                 = "dev-jcs"
